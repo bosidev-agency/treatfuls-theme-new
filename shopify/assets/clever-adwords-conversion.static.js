d293dea440935c6165d3eb6a1b99a4f7bd3c0254
bosidev <info@bosidev.com>
2024-03-06 22:39:34 +0800

    Remove more unnecessary assets

--- shopify/assets/clever-adwords-conversion.static.js
+++
@@ -1,1 +0,0 @@
-new Image(1,1).src = 'https://www.googleadservices.com/pagead/conversion/1088501118/?currency='+Shopify.checkout.currency'/?value='+Shopify.checkout.total_price+'&label=ej5eCJaG6McDEPCtsMYo&guid=ON&oid='+Shopify.checkout.id+'&script=0';
