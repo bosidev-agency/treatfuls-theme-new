349c06f6e1b9480d07db478b70cdb89713c911d1
bosidev <info@bosidev.com>
2024-09-08 20:59:38 +0200

    Improve richtext section

--- src/js/sections/home/richtext.js
+++ src/js/sections/home/richtext.js
@@ -1,20 +1,20 @@
 import { register } from "@shopify/theme-sections";
 
 const selectors = {
-	tooltipImg: ".js-home-tooltip-img",
-	tooltip: ".tooltip"
+  tooltipImg: ".js-home-tooltip-img",
+  tooltip: ".tooltip",
 };
 
-register("home-richtext", {
-	onLoad: function () {
-		this.init();
-	},
-	init: function () {
-		const tooltipImg = this.container.querySelector(selectors.tooltipImg);
-		const tooltip = this.container.querySelector(selectors.tooltip);
+register("richtext", {
+  onLoad: function () {
+    this.init();
+  },
+  init: function () {
+    const tooltipImg = this.container.querySelector(selectors.tooltipImg);
+    const tooltip = this.container.querySelector(selectors.tooltip);
 
-		if (tooltipImg && tooltip) {
-			tooltip.append(tooltipImg);
-		}
-	}
+    if (tooltipImg && tooltip) {
+      tooltip.append(tooltipImg);
+    }
+  },
 });
