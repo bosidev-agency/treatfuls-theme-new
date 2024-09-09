c0b09c832f43dd1823f69b8adeee52a2db80c55f
bosidev <info@bosidev.com>
2024-09-09 13:08:02 +0200

    Improve comparison slider

--- src/js/sections/comparison-slider.js
+++ src/js/sections/comparison-slider.js
@@ -1,24 +1,54 @@
-import $ from "jquery";
+class ComparisonSlider extends HTMLElement {
+  constructor() {
+    super();
 
-const topImage = document.querySelectorAll('.img-comp-img')[1];
-  const slider = document.querySelector('#Custom_slider');    
-  const resizeImg = (e) => {  
-    topImage.style.width = `${e.target.value}px`
+    // Element references
+    this.slider = null;
+    this.topImageWrapper = null;
+    this.beforeImage = null;
   }
 
-  //slider.addEventListener('mousemove', resizeImg)
+  connectedCallback() {
+    this.slider = this.querySelector("#compare-slider-range");
+    this.topImageWrapper = this.querySelector(".comparison-slider__image--top");
+    this.beforeImage = this.querySelector(".comparison-slider__image img");
 
-  $('#Custom_slider_desktop').mousemove(function(e){
-      var max_width = $(this).attr('max');
-      var width = e.target.value;
-      var wid = width+'px';
-      $('.img-comp-img.img-comp-overlay').css('width',wid);
+    // Wait for the images to load before calculating sizes
+    this.beforeImage.onload = () => {
+      this.setDynamicSliderValues();
+    };
 
-   }); 
+    // Add event listener to the slider
+    this.addListeners();
+  }
 
-$('#Custom_slider_mobile').bind('touchmove', function(e) {
-      var max_width = $(this).attr('max');
-      var width = e.target.value;
-      var wid = width+'px';
-      $('.img-comp-img.img-comp-overlay').css('width',wid);
-   }); 
+  disconnectedCallback() {
+    // Clean up if necessary
+  }
+
+  // Add event listener to the slider
+  addListeners() {
+    this.slider.addEventListener("input", this.updateImageWidth.bind(this));
+  }
+
+  // Set the slider max value and default value (50%) based on the image width
+  setDynamicSliderValues() {
+    // Set the max value of the slider to 100 (representing 100%)
+    this.slider.max = 100;
+
+    // Set the value of the slider to 50% (starting at the middle)
+    this.slider.value = 50;
+
+    // Set the initial width of the top image wrapper to 50%
+    this.topImageWrapper.style.width = "50%";
+  }
+
+  // Update the width of the top image wrapper based on the slider value
+  updateImageWidth(e) {
+    const sliderValue = e.target.value;
+    this.topImageWrapper.style.width = `${sliderValue}%`;
+  }
+}
+
+// Define the custom element
+customElements.define("comparison-slider", ComparisonSlider);
