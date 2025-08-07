cc96adb24c1bd3f4f1cb2b9d74086c02af500d32
bosidev <info@bosidev.com>
2025-08-07 19:48:01 +0200

    Improve slider

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,21 +1,31 @@
-import Swiper from 'swiper';
+import Swiper from "swiper";
 
 class SliderElement extends HTMLElement {
   constructor() {
     super();
-    this.sliderConfig = this.dataset.swiperConfig ? JSON.parse(this.dataset.swiperConfig) : {};
+    this.sliderConfig = this.dataset.swiperConfig
+      ? JSON.parse(this.dataset.swiperConfig)
+      : {};
     this.swiperInstance = null;
-    this.mediaQuery = window.matchMedia('(max-width: 991px)');
+    this.mediaQuery = window.matchMedia("(max-width: 991px)");
     this.handleResize = this.handleResize.bind(this);
   }
 
   connectedCallback() {
-    this.handleResize(this.mediaQuery); // Check initial screen size
-    this.mediaQuery.addEventListener('change', this.handleResize); // Add listener for screen size changes
+    // Check if the element has data-only-mobile attribute
+    if (this.hasAttribute("data-only-mobile")) {
+      this.handleResize(this.mediaQuery); // Check initial screen size
+      this.mediaQuery.addEventListener("change", this.handleResize); // Add listener for screen size changes
+    } else {
+      // Initialize slider immediately without mobile-only restriction
+      this.swiperInstance = new Swiper(this, this.sliderConfig);
+    }
   }
 
   disconnectedCallback() {
-    this.mediaQuery.removeEventListener('change', this.handleResize); // Remove listener when element is removed
+    if (this.hasAttribute("data-only-mobile")) {
+      this.mediaQuery.removeEventListener("change", this.handleResize); // Remove listener when element is removed
+    }
     if (this.swiperInstance) {
       this.swiperInstance.destroy(true, true); // Destroy Swiper instance if it exists
     }
@@ -37,4 +47,4 @@ class SliderElement extends HTMLElement {
   }
 }
 
-customElements.define('slider-element', SliderElement);
+customElements.define("slider-element", SliderElement);
